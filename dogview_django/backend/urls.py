7ca0b9169cbccfd5b20451b646cf6e65372ab063
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-11-05 12:06:20 -0500

    cleaned up repository

--- dogview_django/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from backend import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^reads/', views.reads, name='reads'),
-    url(r'^pileups/', views.pileups, name='pileups'),
-    url(r'^expressions/', views.expressions, name='expression'),
-)
--- dogview_django/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def graph(request):
-	# return HttpResponse("Hello, world. You're at the graph.")
-	return render(request, 'frontend/graph.html')
-
